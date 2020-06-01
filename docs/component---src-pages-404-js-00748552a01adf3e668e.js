f83eab0985e7ab2a9c34f20013e8a84e68c50f45
me@jamesmfriedman.com <me@jamesmfriedman.com>
2020-05-31 22:31:00 -0400

    Building docs

---
+++ docs/component---src-pages-404-js-00748552a01adf3e668e.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[14],{w2l6:function(t,e,n){"use strict";n.r(e);var i=n("aD51");e.default=function(){return Object(i.c)("div",{style:{display:"flex",alignItems:"center",justifyContent:"center",height:"100vh",width:"100vw",fontSize:32}},"Not Found")}}}]);
+//# sourceMappingURL=component---src-pages-404-js-00748552a01adf3e668e.js.map
