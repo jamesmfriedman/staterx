4b8cecb2c7467d05067d62e1254a39166440189c
me@jamesmfriedman.com <me@jamesmfriedman.com>
2020-05-31 22:44:31 -0400

    Building docs

--- docs/pong/service-worker.js
+++ docs/pong/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/staterx/hacker-news/precache-manifest.70807886170baf63da63711c987129c1.js"
+  "/precache-manifest.2cc54cf173d0e5598e830535a20e9c56.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/staterx/hacker-news/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
