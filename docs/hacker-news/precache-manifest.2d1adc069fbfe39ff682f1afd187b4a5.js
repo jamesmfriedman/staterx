68804689bd86972e768b11a3d35cc31efa323a1d
me@jamesmfriedman.com <me@jamesmfriedman.com>
2020-06-25 15:30:13 -0400

    Building docs

---
+++ docs/hacker-news/precache-manifest.2d1adc069fbfe39ff682f1afd187b4a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af6bbfdcc1f635a65e6a7a8b2e4b09fe",
+    "url": "/staterx/hacker-news/index.html"
+  },
+  {
+    "revision": "b7e7eefa4bccc3d071f4",
+    "url": "/staterx/hacker-news/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "b96bac9b9024fcf499da",
+    "url": "/staterx/hacker-news/static/css/main.18e93c87.chunk.css"
+  },
+  {
+    "revision": "b7e7eefa4bccc3d071f4",
+    "url": "/staterx/hacker-news/static/js/2.96697b45.chunk.js"
+  },
+  {
+    "revision": "a725466eadfdb5f14defc1c46f7a6a49",
+    "url": "/staterx/hacker-news/static/js/2.96697b45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b96bac9b9024fcf499da",
+    "url": "/staterx/hacker-news/static/js/main.a23c85e0.chunk.js"
+  },
+  {
+    "revision": "f30058216806c47c554c",
+    "url": "/staterx/hacker-news/static/js/runtime-main.d162325f.js"
+  }
+]);
---
+++ docs/pong/precache-manifest.aa3c6183e18f77d4130139e7e2f981b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02f757a3df2eeef57d1a4b2bf0c2a496",
+    "url": "/staterx/pong/index.html"
+  },
+  {
+    "revision": "510f3d64b7477f89a0ab",
+    "url": "/staterx/pong/static/css/main.e943dbd5.chunk.css"
+  },
+  {
+    "revision": "d03f45ca01852dfe16d2",
+    "url": "/staterx/pong/static/js/2.3c266113.chunk.js"
+  },
+  {
+    "revision": "4f4a12233651b0855fd74d4d32936871",
+    "url": "/staterx/pong/static/js/2.3c266113.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "510f3d64b7477f89a0ab",
+    "url": "/staterx/pong/static/js/main.6fa6d57a.chunk.js"
+  },
+  {
+    "revision": "bef31c77ec50a90306c5",
+    "url": "/staterx/pong/static/js/runtime-main.bba747e1.js"
+  }
+]);
